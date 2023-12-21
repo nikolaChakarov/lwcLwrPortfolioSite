465587aa308a5f21dddc597c35a6a0e5db7cf269
Nikola Chakarov <nikola.chakarov.dev@gmail.com>
2023-12-21 17:32:50 +0200

    Added Hero section LWC to Portfolio Site

---
+++ force-app/main/default/lwc/lwr_hero/lwr_hero.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/images';
+
+export default class Lwr_hero extends LightningElement {
+    imageUrl = IMAGES + '/hero.png';
+}
